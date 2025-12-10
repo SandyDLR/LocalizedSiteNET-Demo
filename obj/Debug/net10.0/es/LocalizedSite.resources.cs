a95bff83ea56a79eb04b754213ed0db9f06d53f4
Sandy De la Rosa <sandydelarosa@192.168.1.5>
2025-12-10 14:35:43 -0600

    fix

--- obj/Debug/net10.0/es/LocalizedSite.resources.cs
+++ obj/Debug/net10.0/es/LocalizedSite.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalizedSite")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b159478d85aa041230fb7bbb13613e49cb54fd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfff84f772875d0d0185272f276100b3453c6e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalizedSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalizedSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
